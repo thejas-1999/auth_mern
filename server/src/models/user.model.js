e59499236ca2ebbc03d0ab68d0caf7298d5429f5
thejas-1999 <thejask074@gmail.com>
2024-04-08 12:41:37 +0530

    create a usermodel for users

---
+++ server/src/models/user.model.js
@@ -0,0 +1,25 @@
+import Mongoose from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = Mongoose.Model("User", userSchema);
