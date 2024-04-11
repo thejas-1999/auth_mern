abb6dc31af003935c980c31fe3ff9a402b6602d7
thejas-1999 <thejask074@gmail.com>
2024-04-11 12:58:22 +0530

    create sign in api routes

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,11 +1,18 @@
 import { User } from "../models/user.model.js";
+import {handleErrors} from '../utils/error.js'
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+ 
 
   try {
+   /*  if (!(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(username) && /\d/.test(username) && username.length > 5)) {
+      return res.status(400).json({ message: "Username must contain at least one special character or number and be greater than 5 characters" });
+    } */
     const hashPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
@@ -14,3 +21,29 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  try {
+    const {  email, password } = req.body;
+   
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(handleErrors(401,`User not found`))
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return next(handleErrors(401,`wrong credentials
+      `))
+    }
+
+   
+    const token = jwt.sign({ id: user._id }, "secret");
+    /* res.json({ token, userID: user._id }); */
+    res.status(200).json({message:`Successfully Login`})
+  } catch (error) {
+    next(error);
+  }
+};
+
